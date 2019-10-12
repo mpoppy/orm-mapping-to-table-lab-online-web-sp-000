9c224bea3fd254bc0401e2efb8d8f7de2ad9efde
Marc Poppy <poppy.marc@gmail.com>
2019-10-12 21:32:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,12 @@
 class Student
 
+  attr_accessor :name, :grade
+
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
+#class method create
+
+#save method to save attributes to the db
+
 end
